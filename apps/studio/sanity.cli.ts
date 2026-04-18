223ef89399f57f402f54448d6d2c29db14ebe916
Vinayakp2001 <vinayakpsarthi@gmail.com>
2026-04-18 16:23:24 +0530

    add studio setup, env config, sanity cli

---
+++ apps/studio/sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from "sanity/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: "dody3ak4",
+    dataset: "production",
+  },
+  deployment: {
+    appId: "lxji9vr6i39cqlyzk0glccr6",
+  },
+});
